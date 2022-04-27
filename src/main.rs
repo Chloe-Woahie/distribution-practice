b27bb28fd919421cae81972f980c0b1759d1bed0
Chloe-Woahie <longwelldotpy@gmail.com>
2022-04-27 19:14:01 -0400

    v0.1.1

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,18 @@
-use clap::crate_version;
+use self_update::cargo_crate_version;
 
 fn main() {
     update().expect_err("Program updated, please restart the program!");
     println!(
         "Thanks for downloading this program! Version ({})",
-        crate_version!()
+        cargo_crate_version!()
     );
 }
 
-use self_update::cargo_crate_version;
-
 fn update() -> Result<(), Box<dyn std::error::Error>> {
     let status = self_update::backends::github::Update::configure()
-        .repo_owner("jaemk")
-        .repo_name("self_update")
-        .bin_name("github")
+        .repo_owner("chloe-woahie")
+        .repo_name("distribution-practice")
+        .bin_name("distribution-practice")
         .show_download_progress(true)
         .current_version(cargo_crate_version!())
         .build()?
