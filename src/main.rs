b8540da018c369be9beffcd5654d0cf5c8fe1169
Chloe-Woahie <longwelldotpy@gmail.com>
2022-04-27 20:09:44 -0400

    auto-updater works

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,41 @@
 use self_update::cargo_crate_version;
 
 fn main() {
-    update().unwrap_err();
+    try_update().expect_err("Program updated. Please start it again!");
     println!(
         "Thanks for downloading this program! Version ({})",
         cargo_crate_version!()
     );
 }
 
-fn update() -> Result<(), Box<dyn std::error::Error>> {
-    let status = self_update::backends::github::Update::configure()
+/// If this function returns an Ok, the program updated itself and the binary should likely be restarted
+fn try_update() -> Result<(), Box<dyn std::error::Error>> {
+    let update_system_build_result = self_update::backends::github::Update::configure()
         .repo_owner("chloe-woahie")
         .repo_name("distribution-practice")
         .bin_name("distribution-practice")
         .show_download_progress(true)
         .current_version(cargo_crate_version!())
-        .build()?
-        .update()?;
-    println!("Update status: `{}`!", status.version());
-    Ok(())
+        .build();
+
+    let update_result = match update_system_build_result {
+        Ok(update_system) => update_system.update(),
+        Err(e) => {
+            panic!("Failed to build update system! ({})", e);
+        }
+    };
+
+    match update_result {
+        // If update_result is Ok, then the updater updated the program
+        Ok(_) => return Ok(()),
+        Err(e) => {
+            // Update Errors are allowed because they mean the user decided not to update
+            match e {
+                self_update::errors::Error::Update(_) => {}
+                _ => panic!("Updater failed! ({})", e),
+            }
+
+            return Err("Program did not update.".into());
+        }
+    }
 }
